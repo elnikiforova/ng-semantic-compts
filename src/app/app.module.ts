5ebd5e963151514b2bad912bed79d9f78fb36c8e
Katya <ekaterina.leo.nikiforova@gmail.com>
2021-04-03 14:10:44 +0300

    modules & routes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ElementsModule } from './elements/elements.module';
+import { CollectionsModule } from './collections/collections.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    ElementsModule,
+    CollectionsModule,
     AppRoutingModule
   ],
   providers: [],
